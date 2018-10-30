990c84e419d3804220bfe145266be15052219eec
Augusto Cesar Monteiro Silva <augusto.cms7@gmail.com>
2018-10-30 17:39:07 -0300

    Commentary clarification

--- flaskr/urlinput.py
+++ flaskr/urlinput.py
@@ -30,7 +30,7 @@ def get_input():
             return render_template('processing.html')
         flash(error)
 
-    # sites = sites on database
+    # sites = get sites from database
     sites = None
     if sites is not None:
         # TODO: Get data from database and put on g.sites, g.status, g.callback
