c76873451f95abb559b242472fe555156de6b47c
Augusto Cesar Monteiro Silva <augusto.cms7@gmail.com>
2018-11-05 14:39:20 -0200

    Adding data to database and retrieving on GET request

--- flaskr/urlinput.py
+++ flaskr/urlinput.py
@@ -1,6 +1,7 @@
 from flask import (
     Blueprint, flash, g, render_template, request
 )
+from . import models
 
 bp = Blueprint('input', __name__, url_prefix='/')
 
@@ -8,27 +9,37 @@ bp = Blueprint('input', __name__, url_prefix='/')
 def get_input():
     if request.method == 'POST':
         # Get URLs from JSON
+        from . import db
+        from flask_migrate import migrate, upgrade
+        db.drop_all()
+        migrate()
+        upgrade()
         error = None
         if not request.is_json:
             error = 'Not a JSON file'
         else:
             input_data = request.get_json()
             g.sites = input_data['sites']
+            g.callback = input_data['callback']
             g.status = {}
             for site in g.sites:
                 g.status[site] = 'Not processed'
-            g.callback = input_data['callback']
-            # TODO: Insert on Database
-            # TODO: Start processing URL's
+                url = models.MURL(site)
+                url.save()
+                status = models.MSTATUSTYPE('Not processed')
+                status.save()
+                processment = models.MURLPROCESSMENT(url.id, status.id)
+                processment.save()
 
         if error is None:
             return render_template('processing.html')
         flash(error)
 
-    # sites = get sites from database
-    sites = None
-    if sites is not None:
-        # TODO: Get data from database and put on g.sites, g.status, g.callback
+    urls = models.MURLPROCESSMENT.get_all()
+    if urls is not None:
+        g.sites = [models.MURL.query.get(s.url_id).urlpath for s in urls]
+        g.status = {models.MURL.query.get(s.url_id).urlpath : models.MSTATUSTYPE.query.get(s.status_id).description for s in urls}
+        g.callback = "" #get from DB
         return render_template('processing.html')
 
     return render_template('input.html')
