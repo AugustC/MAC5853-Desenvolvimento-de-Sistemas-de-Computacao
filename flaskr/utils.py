dd07714bb3459854a433f0c3099d71c390d74ea7
Augusto Cesar Monteiro Silva <augusto.cms7@gmail.com>
2018-11-24 12:50:59 -0200

    Added User Agent change feature on get_html

--- flaskr/utils.py
+++ flaskr/utils.py
@@ -1,9 +1,8 @@
-import urllib.request
+from urllib.request import Request, urlopen
 
 def get_html(url):
-    fp = urllib.request.urlopen(url)
-    htmlbytes = fp.read()
+    q = Request(url, headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'})
+    htmlbytes = urlopen(q).read()
 
     html = htmlbytes.decode("utf8", errors='replace')
-    fp.close()
     return html
